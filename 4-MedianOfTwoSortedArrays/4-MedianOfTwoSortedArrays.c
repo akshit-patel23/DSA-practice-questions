f5da41a8d849f47cd9a4d9e35e76e8bb2036ed0e
Akshit Patel <akshitpatel.g@gmail.com>
2026-07-27 15:02:54 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.c
@@ -0,0 +1,41 @@
+// Last updated: 7/27/2026, 3:02:54 PM
+#include <stdio.h>
+#include <limits.h>
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    // This code is written by Anand Soni
+    if (nums1Size > nums2Size) {
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+    }
+
+    int x = nums1Size, y = nums2Size;
+    int low = 0, high = x;
+
+    while (low <= high) {
+        int partitionX = (low + high) / 2;
+        int partitionY = (x + y + 1) / 2 - partitionX;
+
+        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+
+        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            if ((x + y) % 2 == 0) {
+                return (fmax(maxLeftX, maxLeftY) + fmin(minRightX, minRightY)) / 2.0;
+            } else {
+                return fmax(maxLeftX, maxLeftY);
+            }
+        } else if (maxLeftX > minRightY) {
+            high = partitionX - 1;
+        } else {
+            low = partitionX + 1;
+        }
+    }
+    
+    return 0.0; 
+}
+
+
+
