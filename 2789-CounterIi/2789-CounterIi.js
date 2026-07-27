daf518aff8dfa8b424bcecd4841014a6e1d0f1cd
Akshit Patel <akshitpatel.g@gmail.com>
2026-07-27 15:01:28 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,25 @@
+// Last updated: 7/27/2026, 3:01:28 PM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let temp=init;
+    function increment(){
+        return ++temp;
+    }
+    function decrement(){
+        return --temp;
+    }
+    function reset(){
+        return (temp=init);
+    }
+    return {increment , decrement ,reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
