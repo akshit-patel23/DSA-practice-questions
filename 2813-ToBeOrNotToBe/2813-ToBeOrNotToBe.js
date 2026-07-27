acf7aaee138e6ab5e05410808220a2668e0d5477
Akshit Patel <akshitpatel.g@gmail.com>
2026-07-27 15:01:25 +0530

    Create 2813-ToBeOrNotToBe

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,23 @@
+// Last updated: 7/27/2026, 3:01:25 PM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+   return {
+       toBe:(val2)=>{
+           if(val!== val2)throw new Error("Not Equal");
+           else return true;
+       },
+       notToBe:(val2)=>{
+           if(val===val2)throw new Error("Equal")
+           else return true;
+       }
+
+   }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
