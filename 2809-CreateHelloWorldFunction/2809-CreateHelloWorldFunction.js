c6c45d3a79e5a94823c5e013faafb1faa031ad36
Akshit Patel <akshitpatel.g@gmail.com>
2026-07-27 15:01:27 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 7/27/2026, 3:01:26 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
