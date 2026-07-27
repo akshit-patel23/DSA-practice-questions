434fa71daa93c7336087820f2d2aab88dec7987a
Akshit Patel <akshitpatel.g@gmail.com>
2026-07-27 15:01:44 +0530

    Create 1791-RichestCustomerWealth

---
+++ 1791-RichestCustomerWealth/1791-RichestCustomerWealth.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/27/2026, 3:01:44 PM
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int richest=0;
+        for(int i=0;i<accounts.size();i++){
+            int wealth=0;
+            for(int j=0;j<accounts[i].size();j++){
+              wealth=wealth+accounts[i][j];
+            }
+            if(wealth>richest){
+                richest=wealth;
+            }
+        }
+        return richest;
+    }
+};
